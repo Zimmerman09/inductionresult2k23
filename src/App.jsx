c6c117f7496aeb18cd2eca7774868e2831742798
Zimmerman09 <132766842+Zimmerman09@users.noreply.github.com>
2024-02-16 00:05:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Results from './components/results'
+
+
+
+function App() {
+  return (
+    <Results/>
+  )
+}
+
+export default App
---
+++ src/components/results.jsx
@@ -0,0 +1,282 @@
+import React from 'react'
+import './results.css'
+import { useGSAP } from "@gsap/react";
+
+const Results = () => {
+    
+  return (
+    <div className='induction_container'>
+
+      <div className='induction_heading'>
+        WELCOME BATCH 2K23
+      </div>
+      <div className='induction_para'>
+      Team  Revanta welcomes the batch of 2023 <br/>
+       to our team
+      </div>
+      <div className='finalist'>
+
+      <div className='inductees'>
+        <div className='induction_left'>
+          <div className='induction_names'>
+
+        <div>Krishna Agarwal 
+                       
+ </div>
+        <div>Amit Kumar
+ 
+                        </div>
+        <div>Abhay Toppo
+
+                        </div>
+        <div>PALLAB PRATIHAR
+ 
+                        </div>
+       
+        <div>DEEPAK KUMAR
+                         </div>
+        <div> PANKAJ KUMAR
+
+                       </div>
+        <div> PALAK SONY
+
+                        </div>
+        <div>BANOTH YASWITHA 
+                        </div>
+        <div>SONAM KUMARI
+                        </div>
+        <div>KIRAN KUMARI
+
+                        </div>
+        <div>DADI SRI LINEESHA
+
+                         </div>
+        <div>RITIKA RANI
+
+                        </div>
+        <div>SATYAM KUMAR 
+
+                         </div>
+        <div>ANKIT RAJ
+
+                       </div>
+        <div>KURUSENGA ESHWAR 
+
+                        </div>
+        <div> RAVI RANJAN
+
+                        </div>
+        <div>PRAJAKTA SHIDORE
+
+                        </div>
+                        <div>SONU HANSDA
+                        </div>
+          
+          </div>
+          <div className='roll'>
+            <div>2023UGEC051</div>
+            <div>2023UGEC102
+</div>
+            <div>2023UGEC083</div>
+            <div></div>
+            <div>2023UGME004
+</div>
+            <div>2023UGME015
+</div>
+            <div>2023UGME018
+</div>
+            <div>2023UGME020
+</div>
+            <div>2023UGME021
+</div>
+            <div>2023UGME023
+</div>
+            <div>2023UGME026
+</div>
+            <div>2023UGME040
+</div>
+            <div>2023UGME051
+</div>
+            <div>2023UGME057
+</div>
+            <div>2023UGME104
+</div>
+            <div>2023UGME114
+</div>
+            <div>2023UGCM001
+</div>
+            <div>2023UGCM005</div>
+            <div>2023UGCM023</div>
+          </div>
+          </div>
+          <div>
+        </div>
+        
+        <div className='induction_right'>
+          <div className='induction_names'>
+
+       
+        <div>Soumosish Sengupta 
+                        </div>
+        <div>Saksham Sagar
+
+                         </div>
+        <div>Ayush Raghav
+
+                       </div>
+        <div>Hemant Sharma
+                        </div>
+        <div>Shristi Shree
+
+                         </div>
+        <div>Apoorva Jain
+
+                       </div>
+        <div>Ayush Sahu
+
+                         </div>
+        <div>Sidharth Kumar Singh
+
+                        </div>
+        <div> Sohan Kumar Pandit
+
+                       </div>
+        <div> Chandrakant Gorain
+                         </div>
+        <div>Priyanshu Tiwari
+
+                        </div>
+        <div>Malavatu Manikanta
+
+
+                        </div>
+        <div>Animesh Kumar 
+
+
+                        </div>
+        <div>Tabish Ahmad
+
+                         </div>
+        <div>Ashutosh Singh
+
+                        </div>
+        <div>Armaan Srivastava
+
+                       </div>
+        <div>Sunil Kumar
+
+                        </div>
+        <div>Chandan
+
+                       </div>
+        <div>Aayush Kumar
+
+
+                         </div>
+          </div>
+          <div className='roll'>
+            
+            <div>
+            2023UGCE025
+
+
+            </div>
+            <div>
+            2023UGCE062
+
+
+            </div>
+            <div>
+            2023UGEE007
+
+
+            </div>
+            <div>
+            2023UGEE020
+
+
+            </div>
+            <div>
+            2023UGEE048
+ 
+
+            </div>
+            <div>
+            2023UGEE085
+
+
+            </div>
+            <div>
+            2023UGEE091
+
+
+            </div>
+            <div>
+            2023UGEE092
+
+
+            </div>
+            <div>
+            2023UGEE097
+
+
+            </div>
+            <div>
+            2023UGEE098
+
+
+            </div>
+            <div>
+            2023UGEE105
+
+
+            </div>
+            <div>
+            2023UGEE108
+
+
+            </div>
+            <div>
+            2023UGPI016
+
+
+            </div>
+            <div>
+            2023UGPI021
+
+
+            </div>
+            <div>
+            2023UGPI051
+
+
+            </div>
+            <div>
+            2023UGPI052
+
+
+            </div>
+            <div>
+            2023UGPI053
+
+
+            </div>
+            <div>
+            2023UGPI054
+
+
+            </div>
+            <div>
+            2023UGPI031
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Results
